bb22e606f2484bb4c0f15e90cbb473126d27f4c7
Gonçalo Nunes <goncalonunes1111@gmail.com>
2021-04-30 02:58:46 +0100

    Update: navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -133,7 +133,7 @@ export default class Navbar extends Component {
                           "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         )}
                       >
-                        <span>VSphere</span>
+                        <span>VCenter</span>
                         <ChevronDownIcon
                           className={classNames(
                             open ? "text-gray-600" : "text-gray-400",
@@ -224,6 +224,20 @@ export default class Navbar extends Component {
                   className="text-base font-medium text-gray-500 hover:text-gray-900"
                 >
                   Docs
+                </a>
+
+                <a
+                  href="#"
+                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Q&A
+                </a>
+
+                <a
+                  href="/dashboard"
+                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                >
+                  Dashboard
                 </a>
               </Popover.Group>
               {/* CENTER BUTTONS ------------------------------------------------------------- END */}
